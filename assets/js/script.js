bbbd3c1db6671d6eb9fc77f4e35fe793ff76fd7a
cmwschroeder <cmwschroeder@gmail.com>
2022-04-09 11:08:12 -0700

    added cards for upcoming days weather, changed some button styling

--- assets/js/script.js
+++ assets/js/script.js
@@ -29,20 +29,20 @@ var previousCity = function (event) {
 
 function addButton(city) {
     var btn = $("<btn>");
-    btn.addClass("btn btn-primary m-2 w-100");
+    btn.addClass("btn btn-white mx-2 w-100 border border-dark");
     btn.text(city);
     buttonDivEl.append(btn);
 }
 
 function populateWeather(city) {
     var requestUrl = "https://api.openweathermap.org/data/2.5/weather?q=" + city + "&appid=" + apiKey + "&units=imperial";
+    weatherDivEl.html("");
 
     fetch(requestUrl)
     .then(function (response) {
       return response.json();
     })
     .then(function (data) {
-      console.log(data);
       var currWeatherCard = $('<section>');
       currWeatherCard.addClass("card p-3 col-12");
       var hAndIcon = $('<div>');
@@ -50,7 +50,7 @@ function populateWeather(city) {
       var header = $('<h2>');
       header.text(city.replace("+", " ") + " (" + today.format("MM/DD/YYYY") + ")");
       var img = $('<img>');
-      img.attr("src", "http://openweathermap.org/img/wn/" + data.weather[0].icon + ".png")
+      img.attr("src", "http://openweathermap.org/img/wn/" + data.weather[0].icon + ".png");
       var tempEl = $('<p>');
       tempEl.text("Temperature: " + data.main.temp + " °F");
       var humidEl = $('<p>');
@@ -63,7 +63,6 @@ function populateWeather(city) {
         return response.json();
       })
       .then(function (data) {
-        console.log(data);
         var uvEl = $('<p>');
         uvEl.text("UV Index: " + data.current.uvi);
         hAndIcon.append(header);
@@ -74,13 +73,59 @@ function populateWeather(city) {
         currWeatherCard.append(windSpeedEl);
         currWeatherCard.append(uvEl);
         weatherDivEl.append(currWeatherCard);
-        buildFiveDayForecast(city);
+        buildFiveDayForecast(data);
       });
     });
 }
 
-function buildFiveDayForecast(city) {
-  
+function buildFiveDayForecast(data) {
+  //initialize variables to hold the date that we will be populating
+  var year = today.year();
+  var dayOfYear = today.dayOfYear();
+  var writtenDay;
+
+  console.log(data);
+
+  var header = $("<h3>");
+  header.addClass("col-12");
+  header.text("5-Day Forecast: ");
+  weatherDivEl.append(header);
+
+  //for loop to iterate through and add each successive days weather
+  for(var i = 1; i < 6; i++) {
+
+    //make the day of the year plus one
+    dayOfYear++;
+    //check to see if we need to increment the year, also holds logic for leap years
+    if(dayOfYear > 365 && (year % 4) != 0) {
+      dayOfYear = 1;
+      year++;
+    } else if (dayOfYear > 366 && (year % 4) === 0){
+      dayOfYear = 1;
+      year++;
+    }
+
+    //use moment to get the next day
+    writtenDay = moment().dayOfYear(dayOfYear);
+
+    //start creating the div to hold this days weather
+    var currDayDiv = $('<div>');
+    currDayDiv.addClass("col-2 bg-primary m-2 rounded text-light");
+    var header = $('<h3>');
+    header.text("(" + writtenDay.format("MM/DD/YYYY") + ")");
+    var img = $('<img>');
+    img.attr("src", "http://openweathermap.org/img/wn/" + data.daily[i].weather[0].icon + ".png");
+    var tempEl = $('<p>');
+    tempEl.text("Temperature: " + data.daily[i].temp.day + " °F");
+    var humidEl = $('<p>');
+    humidEl.text("Humidity: " + data.daily[i].humidity + "%");
+
+    currDayDiv.append(header);
+    currDayDiv.append(img);
+    currDayDiv.append(tempEl);
+    currDayDiv.append(humidEl);
+    weatherDivEl.append(currDayDiv);
+  }
 }
 
 cityFormEl.on("submit", citySubmit);
