550c99af71c658c2b0d05353e55f5fdefcc6d781
cmwschroeder <cmwschroeder@gmail.com>
2022-04-09 02:24:11 -0700

    added ability to click on past buttons and populate weather, added current weather data to page

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,9 +3,11 @@ var inputtedCityEl = $("#input-lookup-city");
 var cityFormEl = $("#input-form");
 var cityNameEl = $("#input-lookup-city");
 var buttonDivEl = $("#button-div");
+var weatherDivEl = $("#weather-field");
 
 //holds the current city whos weather should be displayed
 var apiKey = "1373a1914a26d8a7e9e30e10d307cd06";
+var today = moment();
 
 /*
  * On submit of the form this function will get the value of the city
@@ -20,6 +22,11 @@ var citySubmit = function (event) {
     populateWeather(currCity.replace(" ", "+"));
 }
 
+var previousCity = function (event) {
+    var currCity = $(event.currentTarget).text();
+    populateWeather(currCity.replace(" ", "+"));
+};
+
 function addButton(city) {
     var btn = $("<btn>");
     btn.addClass("btn btn-primary m-2 w-100");
@@ -28,7 +35,7 @@ function addButton(city) {
 }
 
 function populateWeather(city) {
-    var requestUrl = "https://api.openweathermap.org/data/2.5/weather?q=" + city + "&appid=" + apiKey;
+    var requestUrl = "https://api.openweathermap.org/data/2.5/weather?q=" + city + "&appid=" + apiKey + "&units=imperial";
 
     fetch(requestUrl)
     .then(function (response) {
@@ -36,7 +43,31 @@ function populateWeather(city) {
     })
     .then(function (data) {
       console.log(data);
+      var header = $('<h2>');
+      header.text(city.replace("+", " ") + " (" + today.format("MM/DD/YYYY") + ")");
+      var tempEl = $('<p>');
+      tempEl.text("Temperature: " + data.main.temp + " °F");
+      var humidEl = $('<p>');
+      humidEl.text("Humidity: " + data.main.humidity + "%");
+      var windSpeedEl = $('<p>');
+      windSpeedEl.text("Wind Speed: " + data.wind.speed + " MPH")
+      requestUrl = "https://api.openweathermap.org/data/2.5/onecall?lat=" + data.coord.lat + "&lon=" + data.coord.lon + "&appid=" + apiKey + "&units=imperial";
+      fetch(requestUrl)
+      .then(function (response) {
+        return response.json();
+      })
+      .then(function (data) {
+        console.log(data);
+        var uvEl = $('<p>');
+        uvEl.text("UV Index: " + data.current.uvi);
+        weatherDivEl.append(header);
+        weatherDivEl.append(tempEl);
+        weatherDivEl.append(humidEl);
+        weatherDivEl.append(windSpeedEl);
+        weatherDivEl.append(uvEl);
+      });
     });
 }
 
 cityFormEl.on("submit", citySubmit);
+buttonDivEl.on("click", ".btn", previousCity);
